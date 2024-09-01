52682b218f9dbac1158429fd7668787e364a31ce
Tony <tonyatlcs@gmail.com>
2024-09-01 21:17:44 +1000

    code for amazon data aggregation

--- src/backend/main.py
+++ src/backend/main.py
@@ -6,6 +6,9 @@ if __name__ == "__main__":
   clean_amazon_data = Clean_amazon_data()
 
   amazon_digital_music_path = '../../src/data/raw_data/amazon_data/review/'
+  amazon_digital_music_output_path = '../../src/data/compressed/amazon_data'
+  amazon_output_compressed_file_name = 'amazon_review_data.parquet'
 
-  clean_amazon_data.aggregate_review_data(amazon_digital_music_path)
+
+  clean_amazon_data.aggregate_json_data(amazon_digital_music_path, amazon_digital_music_output_path, amazon_output_compressed_file_name)
   
--- src/backend/scripts/clean_amazon_data.py
+++ src/backend/scripts/clean_amazon_data.py
@@ -5,7 +5,7 @@ class Clean_amazon_data:
   def __init__(self):
     pass
 
-  def aggregate_review_data(self, folder_path: str):
+  def aggregate_json_data(self, folder_path: str, output_path: str, output_file_name: str):
     data_folder_path = folder_path
 
     datframes = []
@@ -22,6 +22,13 @@ class Clean_amazon_data:
         datframes.append(df)
     
     big_df = pd.concat(datframes, ignore_index=True)
-
     print(big_df.head())
     print(big_df.shape)
+
+    if not os.path.exists(output_path):
+      os.makedirs(output_path)
+
+    output_file_path = os.path.join(output_path, output_file_name)
+
+    big_df.to_parquet(output_file_path, index=False, compression='zstd')
+    print(f"Data successfully saved to {output_file_name} with Zstandard compression.")
